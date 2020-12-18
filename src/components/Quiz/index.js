6e60503caec1359f125faf07dcca0b5241cece9c
lenycherry <lenoircelia@hotmail.fr>
2020-12-18 16:11:52 +0100

    fixed: percentaage score. feat: access to next levels, back to the current lvl first question if you failed. style: loader on quizover fail display

--- src/components/Quiz/index.js
+++ src/components/Quiz/index.js
@@ -12,21 +12,28 @@ toast.configure();
 
 class Quiz extends Component {
 
-    state = {
-        levelNames: ['debutant', 'confirme', 'expert'],
-        quizLevel: 0,
-        maxQuestions: 10,
-        storedQuestions: [],
-        question: null,
-        options: [],
-        idQuestion: 0,
-        btnDisabled: true,
-        userAnswer: null,
-        score: 0,
-        showWelcomeMsg: false,
-        quizEnd: false
-    }
+    constructor(props) {
+        super(props)
 
+        this.initialState = {
+            levelNames: ['debutant', 'confirme', 'expert'],
+            quizLevel: 0,
+            maxQuestions: 10,
+            storedQuestions: [],
+            question: null,
+            options: [],
+            idQuestion: 0,
+            btnDisabled: true,
+            userAnswer: null,
+            score: 0,
+            showWelcomeMsg: false,
+            quizEnd: false
+        }
+
+        this.state = this.initialState;
+        
+    }
+    
     storedDataRef = React.createRef();
 
     loadQuestions = quizz => {
@@ -46,7 +53,7 @@ class Quiz extends Component {
         }
     }
 
-    showWelcomeMsg = pseudo => {
+    showToastMsg = pseudo => {
         if (!this.state.showWelcomeMsg) {
             this.setState({
                 showWelcomeMsg: true
@@ -68,7 +75,9 @@ class Quiz extends Component {
 
     nextQuestion = () => {
         if (this.state.idQuestion === this.state.maxQuestions - 1) {
-            this.gameOver();
+            this.setState({
+                quizEnd: true
+            })
         } else {
             this.setState(prevState => ({
                 idQuestion: prevState.idQuestion + 1
@@ -105,17 +114,17 @@ class Quiz extends Component {
     }
 
     componentDidUpdate(prevProps, prevState) {
-        if (this.state.storedQuestions !== prevState.storedQuestions) {
-            //si on détecte une update de storequestions
+        if ((this.state.storedQuestions !== prevState.storedQuestions) && this.state.storedQuestions.length) {
+            //si on détecte une update de storequestions et que celui ci n'est pas vide
             //on va mettre à jour les states questions et options
             this.setState({
                 question: this.state.storedQuestions[this.state.idQuestion].question,
                 options: this.state.storedQuestions[this.state.idQuestion].options
             })
         }
-        //si l'id de la question est différent du précédent alors on met à jour les states question et options 
+        //si l'id de la question est différent du précédent et que le storedquestions n'est pas vide alors on met à jour les states question et options 
         //et on reset les states du bouton et de la sélection de réponse à leur valeur d'origine.
-        if (this.state.idQuestion !== prevState.idQuestion) {
+        if ((this.state.idQuestion !== prevState.idQuestion) && this.state.storedQuestions.length){
             this.setState({
                 question: this.state.storedQuestions[this.state.idQuestion].question,
                 options: this.state.storedQuestions[this.state.idQuestion].options,
@@ -123,9 +132,14 @@ class Quiz extends Component {
                 btnDisabled: true
             })
         }
-        //si on a récup le pseudo utilisateur, on invoque la méthode showWelcomeMsg qui va afficher une notification.
-        if (this.props.userData.pseudo) {
-            this.showWelcomeMsg(this.props.userData.pseudo)
+        // mise à jour du score si celui-ci est diff du score précédent via la méthode gameOver.
+        if ( this.state.quizEnd !== prevState.quizEnd ) {
+            const gradepercent = this.getPercentage(this.state.maxQuestions, this.state.score);
+            this.gameOver(gradepercent);
+        }
+        //si le pseudo utilisateur est différent du props précédent, on invoque la méthode showToastMsg qui va afficher une notification.
+        if (this.props.userData.pseudo !== prevProps.userData.pseudo) {
+            this.showToastMsg(this.props.userData.pseudo)
         }
     }
 
@@ -137,10 +151,24 @@ class Quiz extends Component {
             btnDisabled: false
         })
     }
-    gameOver = () => {
-        this.setState({
-            quizEnd: true
-        })
+
+    getPercentage = (maxQuestions, ourScore) => (ourScore / maxQuestions) * 100;
+
+    gameOver = percent => {
+
+        if (percent >= 50) {
+            this.setState({
+                quizLevel: this.state.quizLevel + 1,
+                percent
+            })
+        } else {
+            this.setState({percent})
+        }
+    }
+
+    loadLevelQuestions = param => {
+        this.setState({ ...this.initialState, quizLevel: param })
+        this.loadQuestions(this.state.levelNames[param])
     }
 
     render() {
@@ -160,8 +188,15 @@ class Quiz extends Component {
             )
         })
         //Si le state de quizEnd est true, on affiche le composant QuizOver, sinon on affiche les composants du quiz.
-        return !this.state.quizEnd ? (
-            <QuizOver ref={this.storedDataRef}  />
+        return this.state.quizEnd ? (
+            <QuizOver ref={this.storedDataRef}
+                levelNames={this.state.levelNames}
+                score={this.state.score}
+                maxQuestions={this.state.maxQuestions}
+                quizLevel={this.state.quizLevel}
+                percent={this.state.percent}
+                loadLevelQuestions={this.loadLevelQuestions}
+            />
         )
             :
             (
@@ -173,11 +208,11 @@ class Quiz extends Component {
 
                     { displayOptions}
 
-                    <button onClick={() => this.nextQuestion()} 
-                    disabled={this.state.btnDisabled} 
-                    className='btnSubmit'
+                    <button onClick={() => this.nextQuestion()}
+                        disabled={this.state.btnDisabled}
+                        className='btnSubmit'
                     >
-                        {this.state.idQuestion < this.state.maxQuestions -1 ? 'Suivant' : 'Terminer'}
+                        {this.state.idQuestion < this.state.maxQuestions - 1 ? 'Suivant' : 'Terminer'}
                     </button>
                 </Fragment>
             )
--- src/components/QuizOver/index.js
+++ src/components/QuizOver/index.js
@@ -2,34 +2,97 @@ import React, { Fragment, useEffect, useState } from 'react'
 
 const QuizOver = React.forwardRef((props, ref) => {
 
+    const { levelNames,
+        score,
+        maxQuestions,
+        quizLevel,
+        percent,
+        loadLevelQuestions
+     }
+        = props
+
     const [asked, setAsked] = useState([]);
 
     useEffect(() => {
         setAsked(ref.current)
     }, [ref])
 
-    const questionAnswer = asked.map(question => {
-        return (
-            <tr key={question.id}>
-                <td>{question.question}</td>
-                <td>{question.answer}</td>
-                <td>
-                <button className='btnInfo'>Infos</button>
+    const averageGrade = maxQuestions / 2;
+
+    if(score < averageGrade){
+        setTimeout(() => {
+            loadLevelQuestions(quizLevel)
+        }, 3000);
+    }
+
+    const decision = score >= averageGrade ? (
+        <Fragment>
+            <div className='stepsBtnContainer'>
+                {
+                    quizLevel < levelNames.length ?
+                        (
+                            <Fragment>
+                                <p className='successMsg'>Bravo, passez au niveau suivant !</p>
+                                <button className='btnResult successMsg' onClick={() => loadLevelQuestions(quizLevel)}>Niveau suivant</button>
+                            </Fragment>
+                        )
+                        :
+                        (
+                            <Fragment>
+                                <p className='successMsg'>Bravo, vous êtes un expert !</p>
+                                <button className='btnResult gameOver' onClick={() => loadLevelQuestions(0)}>Accueil</button>
+                            </Fragment>
+                        )
+                }
+            </div>
+            <div className='percentage'>
+                <div className='progressPercent'>Réussite: {percent}%</div>
+                <div className='progressPercent'>Note: {score}/{maxQuestions}</div>
+            </div>
+        </Fragment>
+    )
+        :
+        (
+            <Fragment>
+                <div className='stepsBtnContainer'>
+                    <p className='failureMsg'>Vous avez échoué ...</p>
+                </div>
+                <div className='percentage'>
+                    <div className='progressPercent'>Réussite: {percent} %</div>
+                    <div className='progressPercent'>Note: {score}/{maxQuestions}</div>
+                </div>
+            </Fragment>
+        )
+
+    const questionAnswer = score >= averageGrade ? (
+        asked.map(question => {
+            return (
+                <tr key={question.id}>
+                    <td>{question.question}</td>
+                    <td>{question.answer}</td>
+                    <td>
+                        <button className='btnInfo'>Infos</button>
+                    </td>
+                </tr>
+            )
+        })
+    )
+        :
+        (
+            <tr>
+                <td colSpan="3">
+                    <div className='loader'></div>
+                    <p style={{textAlign:'center', color: 'red'}}>Pas de réponses</p>
                 </td>
             </tr>
         )
-    })
+
 
     return (
         <Fragment>
-            <div className='stepsBtnContainer'>
-                <p className='successMsg'>Bravo, vous êtes un expert !</p>
-                <button className='btnResult successMsg'>Niveau suivant</button>
-            </div>
-            <div className='percentage'>
-                <div className='progressPercent'>Réussite: 10%</div>
-                <div className='progressPercent'>Note: 10/10</div>
-            </div>
+
+            { decision}
+
             <hr />
             <p>Les réponses aux questions posées:</p>
 
